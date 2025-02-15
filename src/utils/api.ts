92af256494fa83a8dbc8963f646b80de9a84fd67
PranavPatel <pranav.patel@kristasoft.com>
2025-02-15 23:14:07 +0530

    commit message

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,9 +12,9 @@ import superjson from "superjson";
 import { type AppRouter } from "~/server/api/root";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative URL
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use Vercel URL
-  return `http://localhost:${process.env.PORT ?? 3000}`; // Dev SSR should use localhost
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 /** A set of type-safe react-query hooks for your tRPC API. */
@@ -33,16 +33,9 @@ export const api = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-          fetch: async (url, options) => {
-            return fetch(url, {
-              ...options,
-              headers: new Headers({ ...(options?.headers || {}) }), // Ensure Headers object
-            });
-          },
           transformer: superjson,
+          url: `${getBaseUrl()}/api/trpc`,
         }),
-        ,
       ],
     };
   },
